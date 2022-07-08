4e80a9bb71f917d218e4be3506b15e87349588f4
Jeffrey <jeffrey.graham600@gmail.com>
2022-07-08 16:57:03 -0400

    Added features allowing users more interaction

--- src/App.js
+++ src/App.js
@@ -1,32 +1,74 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import MemeInputs from "./components/MemeInputs";
 import MemeButton from "./components/MemeButton";
 import Image from "./components/Image";
+
 function App() {
-  const [meme, setMeme] = React.useState("");
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    memeImage: "",
+  });
   const [allMemes, setAllMemes] = React.useState("");
 
   React.useEffect(() => {
     fetch("https://api.imgflip.com/get_memes")
       .then((res) => res.json())
       .then((data) => setAllMemes(data));
-    console.log("render");
   }, []);
 
   function handleClick() {
     let memesArray = allMemes.data.memes;
     let randomNumber = Math.floor(Math.random() * 100);
-    setMeme(memesArray[randomNumber].url);
+    setMeme((prevMeme) => {
+      return {
+        topText: "",
+        bottomText: "",
+        memeImage: memesArray[randomNumber].url,
+      };
+    });
+  }
+
+  function onChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
   }
 
   return (
     <div className="pageContainerOutterPage">
       <NavBar />
       <div className="bottomHalfContainer">
-        <MemeInputs />
+        <main className="memeContainer">
+          <div className="inputContainer">
+            <input
+              onChange={onChange}
+              value={meme.topText}
+              name="topText"
+              type="text"
+              placeholder="Top Text"
+            />
+          </div>
+          <div className="inputContainer">
+            <input
+              onChange={onChange}
+              value={meme.bottomText}
+              name="bottomText"
+              type="text"
+              placeholder="Bottom Text"
+            />
+          </div>
+        </main>
         <MemeButton clickFunction={handleClick} />
-        <Image meme={meme} />
+        <Image
+          meme={meme.memeImage}
+          topText={meme.topText}
+          bottomText={meme.bottomText}
+        />
       </div>
     </div>
   );
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,9 @@
 export default function Image(props) {
   return (
     <div className="imageContainer">
+      <p className="text top">{props.topText}</p>
       <img className="memeImage" src={props.meme} alt="" />
+      <p className="text bottom">{props.bottomText}</p>
     </div>
   );
 }
